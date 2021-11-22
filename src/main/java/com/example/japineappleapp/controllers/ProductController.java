d333a831d47fe941ce0db1e8a6e821db25bb8a5b
Maria L. Apaza Chavez <mb.lev17@gmail.com>
2021-11-22 10:30:12 -0500

    Error CORS (#5)
    Co-authored-by: almalaura <50027180+almalaura@users.noreply.github.com>

--- src/main/java/com/example/japineappleapp/controllers/ProductController.java
+++ src/main/java/com/example/japineappleapp/controllers/ProductController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.japineappleapp.models.Product;
 import com.example.japineappleapp.repositories.ProductRepository;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class ProductController {
