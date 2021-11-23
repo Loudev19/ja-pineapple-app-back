2c1ac3a318f6ab574898ed7004802b2d998bc262
MBlev <loudev07@gmail.com>
2021-11-22 19:00:24 -0500

    Return empty when delete

--- src/main/java/com/example/japineappleapp/controllers/UserController.java
+++ src/main/java/com/example/japineappleapp/controllers/UserController.java
@@ -79,7 +79,7 @@ public class UserController {
     public ResponseEntity<HttpStatus> eliminaUser(@PathVariable(value="id") Integer id) {
     	try {
             userService.delete(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
